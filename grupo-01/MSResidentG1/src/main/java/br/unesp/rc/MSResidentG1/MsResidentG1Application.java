14476f9d9f4d16914590fe32ab0cec61b3b6b67c
brendomota <brendo.saescola@gmail.com>
2025-07-04 14:22:37 -0300

    feat(MSResidentG1): new microservice for resident MSResidentG1.

---
+++ grupo-01/MSResidentG1/src/main/java/br/unesp/rc/MSResidentG1/MsResidentG1Application.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.MSResidentG1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsResidentG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsResidentG1Application.class, args);
+	}
+
+}
---
+++ grupo-01/MSResidentG1/src/test/java/br/unesp/rc/MSResidentG1/MsResidentG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.MSResidentG1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsResidentG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
